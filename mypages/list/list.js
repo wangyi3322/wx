fb67ed795b027f87603ab22eb46babf0f0169339
webyi <1043322514@qq.com>
2018-02-05 11:43:39 +0800

    updata

--- mypages/list/list.js
+++ mypages/list/list.js
@@ -1,3 +1,14 @@
 Page({
-  
+  data:{
+      listArr = []
+  },
+  onReachBottom(){
+      alert('is bottom');
+  },
+  updataArrList(){
+      let arr = this.data.listArr;
+  },
+  createData(){
+      let length = this.data.listArr.left;
+  }
 })
