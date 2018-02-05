2ac51f22b338fe61e54bdb8279fee69106b21138
webyi <1043322514@qq.com>
2018-02-05 11:57:00 +0800

    updata

--- mypages/list/list.js
+++ mypages/list/list.js
@@ -7,8 +7,14 @@ Page({
   },
   updataArrList(){
       let arr = this.data.listArr;
+      arr.push(...createData())
+      this.setData(
+          arrList:arr
+      )
   },
   createData(){
-      let length = this.data.listArr.left;
+      let length = this.data.listArr.length;
+      if(length >= 30 ) return [];
+      return Array.from({length: 3},(v, i)=>`数据${1+i+length}`)
   }
 })
